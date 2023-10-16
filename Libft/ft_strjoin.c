8b11863bbe4ebfc3d4d1336a926390fdf8865d30
PC fixe <dimitri@Darios>
2023-10-16 13:59:43 +0200

    commit du 16/10/2023

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -1,37 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dconza <dconza@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/16 12:18:17 by dconza            #+#    #+#             */
-/*   Updated: 2023/10/16 12:45:35 by dconza           ###   ########.fr       */
+/*   Created: 2023/10/16 13:29:04 by dconza            #+#    #+#             */
+/*   Updated: 2023/10/16 13:29:14 by dconza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start,size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	i;
-	size_t	j;
+	int		i;
+	int		j;
 	char	*str;
 
-	str = (char *)malloc(sizeof(*s)*(len+1));
-	if (!str)
-		return(NULL);
 	i = 0;
 	j = 0;
-	while ( s[i] != '\0')
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
 	{
-		if ( i >= start && j < len)
-		{
-			str[j] = s[i];
-			j++;
-		}
+		str[i] = s1[i];
 		i++;
 	}
-	str[j] = '\0';
-	return(str);
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
 }
