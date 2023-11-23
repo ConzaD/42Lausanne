d40928c69b2d9e9be1432324fa0a63fa8d74b9b0
Dimitri Conza <dconza@c1r5s8.42lausanne.ch>
2023-11-23 11:14:11 +0100

    mise en place du buffer size

--- Get_next_line/get_next_line.h
+++ Get_next_line/get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: dconza <dconza@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 12:06:48 by dconza            #+#    #+#             */
-/*   Updated: 2023/10/21 17:43:34 by dconza           ###   ########.fr       */
+/*   Updated: 2023/11/23 11:13:04 by dconza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 666
+# endif
 
 # include <stdlib.h>
 # include <unistd.h>
