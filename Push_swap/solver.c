83867bbc82768edc6a3ef9a727d29c99e1d091bc
PC fixe <dimitri@Darios>
2023-11-27 11:06:28 +0100

    mise en place de fonctions utiles pour l'algo

---
+++ Push_swap/solver.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solver.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dconza <dconza@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 11:00:09 by dconza            #+#    #+#             */
+/*   Updated: 2023/11/27 11:02:59 by dconza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	calculate_rotation_one(t_stack *a, t_stack *b, int c)
+{
+	int	i;
+
+	i = ft_find_place_b(b, c);
+	if (i < ft_find_index(a, c))
+		i = ft_find_index(a, c);
+	return (i);
+}
+
+int	calculate_rotation_two(t_stack *a, t_stack *b, int c)
+{
+	int	i;
+
+	i = 0;
+	if (ft_find_place_b(b, c))
+		i = ft_lstsize(b) - ft_find_place_b(b, c);
+	if ((i < (ft_lstsize(a) - ft_find_index(a, c))) && ft_find_index(a, c))
+		i = ft_lstsize(a) - ft_find_index(a, c);
+	return (i);
+}
+
+int	calculate_rotation_one_bis(t_stack *a, t_stack *b, int c)
+{
+	int	i;
+
+	i = 0;
+	if (ft_find_index(a, c))
+		i = ft_lstsize(a) - ft_find_index(a, c);
+	i = ft_find_place_b(b, c) + i;
+	return (i);
+}
+
+int	calculate_rotation_two_bis(t_stack *a, t_stack *b, int c)
+{
+	int	i;
+
+	i = 0;
+	if (ft_find_place_b(b, c))
+		i = ft_lstsize(b) - ft_find_place_b(b, c);
+	i = ft_find_index(a, c) + i;
+	return (i);
+}
