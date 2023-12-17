4749043eeecc323f1bf3fbe56dfa452d6ab6d49c
ConzaD <dimitri.conza@gmail.com>
2023-12-17 22:16:38 +0100

    grosse mise a jour de tout

---
+++ Push_swap/libft/ft_strlcpy.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dconza <dconza@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 12:30:42 by dconza            #+#    #+#             */
+/*   Updated: 2023/10/13 17:13:40 by dconza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size == 0)
+	{
+		while (src[i])
+			i++;
+		return (i);
+	}
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < size)
+		dest[i] = '\0';
+	while (src[i] != '\0')
+		i++;
+	return (i);
+}
---
+++ Push_swap/push_swap/ft_list_args.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_args.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dconza <dconza@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/22 18:12:47 by yogun             #+#    #+#             */
+/*   Updated: 2023/12/17 22:10:56 by dconza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+void	list_args(char **argv, t_stack **stack_a)
+{
+	long	i;
+
+	i = 1;
+	while (argv[i] != NULL)
+	{
+		ft_add_back(stack_a, ft_stack_new(ft_atoi(argv[i])));
+		i++;
+	}
+}
