973a4e2b0082b146cfcc559df923f8e155ae7c31
ConzaD <dimitri.conza@gmail.com>
2024-04-16 09:54:35 +0200

    bim bam boum

--- Fract-ol/sources/render.c
+++ Fract-ol/sources/render.c
@@ -6,8 +6,51 @@
 /*   By: dconza <dconza@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 16:16:08 by dconza            #+#    #+#             */
-/*   Updated: 2024/04/11 16:16:10 by dconza           ###   ########.fr       */
+/*   Updated: 2024/04/16 09:39:58 by dconza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+static void	set_pixel_color(t_fractol *f, int x, int y, int color)
+{
+		f->buf[x * 4 + y * WIDTH * 4] = color;
+		f->buf[x * 4 + y * WIDTH * 4 + 1] = color >> 8;
+		f->buf[x * 4 + y * WIDTH * 4 + 2] = color >> 16;
+		f->buf[x * 4 + y * WIDTH * 4 + 3] = color >> 24;
+}
+
+static int	calculate_fractal(t_fractol *f, double pr, double pi)
+{
+	int	nb_iter;
+
+	if (f->set == MANDELBROT)
+		nb_iter = mandelbrot(pr, pi);
+	else if (f->set == JULIA)
+		nb_iter = julia(f, pr, pi);
+	return (nb_iter);
+}
+
+void	render(t_fractol *f)
+{
+	int		x;
+	int		y;
+	double	pr;
+	double	pi;
+	int		nb_iter;
+
+	mlx_clear_window(f->mlx, f->win);
+	y = -1;
+	while (++y < HEIGHT)
+	{
+		x = -1;
+		while (++x < WIDTH)
+		{
+			pr = f->min_r + (double)x * (f->max_r - f->min_r) / WIDTH;
+			pi = f->max_i + (double)y * (f->min_i - f->max_i) / HEIGHT;
+			nb_iter = calculate_fractal(f, pr, pi);
+			set_pixel_color(f, x, y, f->palette[nb_iter]);
+		}
+	}
+	mlx_put_image_to_window(f->mlx, f->win, f->img, 0, 0);
+}
