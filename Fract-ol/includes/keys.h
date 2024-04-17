c4eabf45230d1577979199d1394a3f891fcd97c3
Dimitri Conza <dconza@c1r15s6.42lausanne.ch>
2024-04-17 17:11:08 +0200

    problemes de raccourcis regler

--- Fract-ol/includes/keys.h
+++ Fract-ol/includes/keys.h
@@ -6,7 +6,7 @@
 /*   By: dconza <dconza@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 19:37:47 by dconza            #+#    #+#             */
-/*   Updated: 2024/04/17 16:25:23 by dconza           ###   ########.fr       */
+/*   Updated: 2024/04/17 16:55:31 by dconza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 # define KEYS_H
 
 # define EVENT_CLOSE_BTN 17
-# define KEY_ESC 65307
-# define KEY_W 'w'
-# define KEY_A 97
-# define KEY_S 115
-# define KEY_D 100
-# define KEY_UP 65362
-# define KEY_DOWN 65364
-# define KEY_LEFT 65361
-# define KEY_RIGHT 65363
-# define KEY_PLUS 61
-# define KEY_MINUS 45
-# define KEY_SPACE 32
-# define KEY_ONE 49
-# define KEY_TWO 50
+# define KEY_ESC 53
+# define KEY_W 13
+# define KEY_A 0
+# define KEY_S 1
+# define KEY_D 2
+# define KEY_UP 126
+# define KEY_DOWN 125
+# define KEY_LEFT 123
+# define KEY_RIGHT 124
+# define KEY_PLUS 69
+# define KEY_MINUS 78
+# define KEY_SPACE 49
+# define KEY_ONE 18
+# define KEY_TWO 19
 # define MOUSE_WHEEL_UP 5
 # define MOUSE_WHEEL_DOWN 4
 # define MOUSE_WHEEL_BTN 3
 # define MOUSE_BTN 1
-# define MOUSE_BTN_2 3
-# define KEY_K 107
+# define MOUSE_BTN_2 2
+# define KEY_K 40
 
 #endif
