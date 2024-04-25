ccc09a506dbc2abe2390157f183d7e4bc1397e2a
ConzaD <dimitri.conza@gmail.com>
2024-04-25 22:33:01 +0200

    commencement

---
+++ Philosophers/sources/philo.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dconza <dconza@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 18:22:06 by dconza            #+#    #+#             */
+/*   Updated: 2024/04/25 18:42:51 by dconza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+int	check_arg_content(char *arg)
+{
+	int	i;
+
+	i = 0;
+	while (arg[i] != '\0')
+	{
+		if (arg[i] < '0' || arg[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	check_valid_arg(char **argv)
+{
+	if (ft_atoi(argv[1]) > PHILO_MAX || ft_atoi(argv[1]) <= 0
+		|| check_arg_content(argv[1]) == 1)
+		return (write(2, "Invalid\n", 29), 1);
+	if (ft_atoi(argv[2]) <= 0 || check_arg_content(argv[2]) == 1)
+		return (write(2, "Invalid\n", 21), 1);
+	if (ft_atoi(argv[3]) <= 0 || check_arg_content(argv[3]) == 1)
+		return (write(2, "Invalid\n", 21), 1);
+	if (ft_atoi(argv[4]) <= 0 || check_arg_content(argv[4]) == 1)
+		return (write(2, "Invalid\n", 23), 1);
+	if (argv[5] && (ft_atoi(argv[5]) < 0 || check_arg_content(argv[5]) == 1))
+		return (write(2, "Invalid\n",
+				51), 1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_program		program;
+	t_philo			philos[PHILO_MAX];
+	pthread_mutex_t	forks[PHILO_MAX];
+
+	return	(0);
+}
